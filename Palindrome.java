6631acc67996371eab486509a9a606c31c028225
tousif31 <mohammedtousif3709@gmail.com>
2025-07-28 11:26:22 +0530

    Palindrome added

--- Palindrome.java
+++ Palindrome.java
@@ -1,24 +1,43 @@
-// write a program to check giveen number is palindrome or not.
-import java.util.Scanner;
+// write a program to check giveen numbers is palindrome using keyworld revese using ternary operator
+// Program to check whether a given number how many even and odd numbers are there
+// and also check whether the number is palindrome or not.
 public class Palindrome {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        int reverse = 0;
-        while (number != 0) {
-            int digit = number % 10; // Get the last digit
-            reverse = reverse * 10 + digit; // Build the reversed number
-            number /= 10; // Remove the last digit from the original number
-            
-        }
-        System.out.println("Reversed number: " + reverse);
-        if (reverse == number) {
-            System.out.println("The number is a palindrome.");
-        } else {
-            System.out.println("The number is not a palindrome.");
-        }       
-        scanner.close();
-}
+        int num = 12345; // Example number
+        boolean isPalindrome = isPalindrome(num);
+        System.out.println(num + " is " + (isPalindrome ? "" : "not ") + "a palindrome.");
+        countEvenOdd(num);
+        
     }
+
+    public static boolean isPalindrome(int num) {
+        int reversed = 0;
+        int original = num;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reversed;
+    }
+    public static void countEvenOdd(int num) {
+        int evenCount = 0;
+        int oddCount = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+            num /= 10;
+        }
+
+        System.out.println("Even digits: " + evenCount);
+        System.out.println("Odd digits: " + oddCount);
+    }
+}
